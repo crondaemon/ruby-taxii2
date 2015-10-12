80469b0277247c663201bc2a92ae59b61f654c03
Ryan Breed <opensource@breed.org>
2015-10-11 21:27:39 -0500

    definitely prefer gyoku to builder; poll client works

--- spec/lib/taxii/poll_client_spec.rb
+++ spec/lib/taxii/poll_client_spec.rb
@@ -4,8 +4,8 @@ describe Taxii::PollClient do
   context 'initializing instances'
   context 'generating requests' do
 
-    let(:discovery_req) { '<taxii_11:Discovery_Request xmlns:taxii="http://taxii.mitre.org/messages/taxii_xml_binding-1" xmlns:taxii_11="http://taxii.mitre.org/messages/taxii_xml_binding-1.1" xmlns:tdq="http://taxii.mitre.org/query/taxii_default_query-1" message_id="100"/>' }
-    let(:feed_info)     {'<taxii:Feed_Information_Request xmlns:taxii="http://taxii.mitre.org/messages/taxii_xml_binding-1" xmlns:taxii_11="http://taxii.mitre.org/messages/taxii_xml_binding-1.1" xmlns:tdq="http://taxii.mitre.org/query/taxii_default_query-1" message_id="100"/>'}
+    let(:discovery_req) { '<taxii_11:Discovery_Request     xmlns:taxii="http://taxii.mitre.org/messages/taxii_xml_binding-1" xmlns:taxii_11="http://taxii.mitre.org/messages/taxii_xml_binding-1.1" xmlns:tdq="http://taxii.mitre.org/query/taxii_default_query-1" message_id="100"/>' }
+    let(:feed_info)     { '<taxii:Feed_Information_Request xmlns:taxii="http://taxii.mitre.org/messages/taxii_xml_binding-1" xmlns:taxii_11="http://taxii.mitre.org/messages/taxii_xml_binding-1.1" xmlns:tdq="http://taxii.mitre.org/query/taxii_default_query-1" message_id="100"/>'}
     it 'generates a valid discovery request' do
 
     end
