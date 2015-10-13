17b5204ae7170d195ee6e3f52cf0565826c38d9d
Ryan Breed <opensource@breed.org>
2015-10-13 17:22:00 -0500

    adding vcr

--- ruby-taxii.gemspec
+++ ruby-taxii.gemspec
@@ -31,4 +31,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'guard-rspec'
   spec.add_development_dependency 'guard-bundler'
   spec.add_development_dependency 'simplecov'
+  spec.add_development_dependency 'vcr'
+  spec.add_development_dependency 'webmock'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,19 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'pry'
+
+require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |v|
+  v.cassette_library_dir = 'spec/fixtures/cassettes'
+  v.hook_into :webmock
+end
+
+RSpec.configure do |c|
+  c.extend VCR::RSpec::Macros
+end
+
 require 'simplecov'
 SimpleCov.start
 
 require 'taxii'
-require 'pry'
