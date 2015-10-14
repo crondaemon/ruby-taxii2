f94662335d52864bbf5d5ab8d692db0eccd4cba7
Ryan Breed <opensource@breed.org>
2015-10-13 22:39:46 -0500

    these can always be squashed

---
+++ lib/taxii/parsers/sax_poll_response.rb
@@ -0,0 +1,30 @@
+module Taxii
+  module MessageParsers
+    class PollResponse < Nokogiri::XML::SAX::Document
+      def package_data
+        @package_data ||= []
+      end
+      def capture
+        @capture.nil? ? @capture = false : @capture
+      end
+      def capture=(flag)
+        @capture=flag
+      end
+      def start_element(name, attrs=[])
+        if name.match(/taxii_11:Content_Block/)
+          STDERR.puts 'START Content_Block'
+          self.capture=true
+        end
+      end
+      def characters(str)
+        self.package_data << str
+      end
+      def end_element(name, attrs=[])
+        if name.match(/taxii_11:Content_Block/)
+          STDERR.puts 'END Content_Block'
+          self.capture=false
+        end
+      end
+    end
+  end
+end
